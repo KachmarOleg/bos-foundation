f2cf90356acf151c2b667a38b938c4fd4a5a9dae
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2025-07-23 15:00:23 +0200

    Steps - done

---
+++ tpl-acf-blocks/steps/steps.php
@@ -0,0 +1,22 @@
+<?php $steps = get_field( 'steps' ); ?>
+
+<div class="block__steps">
+    <div class="container is_10">
+        <div class="container_grid">
+            <div class="is_10 sm_start_1_cols_12">
+                <h2><?php echo esc_html($steps['title']); ?></h2>
+
+                <?php if ( $steps['items'] ) : ?>
+                    <div class="step_items">
+		                <?php foreach ($steps['items'] as $step) : ?>
+                            <div class="step_item">
+                                <h3 class="step_item__title h4"><?php echo esc_html($step['title']); ?></h3>
+                                <p><?php echo esc_html($step['description']); ?></p>
+                            </div>
+		                <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
