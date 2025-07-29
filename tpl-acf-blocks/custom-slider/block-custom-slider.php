5cdc5c805033cb55e5c3564d33aeb629ac9b1527
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2025-07-29 15:32:25 +0200

    Slider - done

--- tpl-acf-blocks/custom-slider/block-custom-slider.php
+++ tpl-acf-blocks/custom-slider/block-custom-slider.php
@@ -7,23 +7,29 @@ $custom_slider = get_field( 'custom_slider' );
 if ( $custom_slider ) :
 	?>
     <div class="block__custom_slider">
-        <div class="swiper">
-            <div class="swiper-wrapper">
-				<?php foreach ( $custom_slider as $image ) : ?>
-                    <div class="block__custom_slider_item swiper-slide">
-                        <picture>
-                            <source media="(max-width: 480px)" srcset="<?php echo wp_get_attachment_image_url( $image['id'], 'mob_slider' ); ?>">
-                        	<?php echo wp_get_attachment_image( $image['id'], 'top_default', false, array( 'alt' => get_alt( $image['id'] ), 'class' => 'object_fit' ) ); ?>
-                        </picture>
+        <div class="container">
+            <div class="container_grid">
+                <div class="start_2_cols_10 sm_start_1_cols_12">
+                    <div class="swiper">
+                        <div class="swiper-wrapper">
+				            <?php foreach ( $custom_slider as $image ) : ?>
+                                <div class="block__custom_slider_item swiper-slide">
+                                    <picture>
+                                        <source media="(max-width: 480px)" srcset="<?php echo wp_get_attachment_image_url( $image['id'], 'mob_slider' ); ?>">
+							            <?php echo wp_get_attachment_image( $image['id'], 'top_default', false, array( 'alt' => get_alt( $image['id'] ), 'class' => 'object_fit' ) ); ?>
+                                    </picture>
+                                </div>
+				            <?php endforeach; ?>
+                        </div>
                     </div>
-				<?php endforeach; ?>
-            </div>
-        </div>
 
-        <div class="sw_controls">
-            <div class="sw_prev i_chevron_left"></div>
-            <div class="sw_pagination"></div>
-            <div class="sw_next i_chevron_right"></div>
+                    <div class="sw_controls">
+                        <div class="sw_prev i_chevron_left"></div>
+                        <div class="sw_pagination"></div>
+                        <div class="sw_next i_chevron_right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 <?php endif; ?>
